a6e57edddad1adb4a928670256479aafb7fc0ae2
lyzgeorge <lyzgeorge@gmail.com>
2025-05-23 08:04:29 +0000

    modified:   .idx/dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,14 +5,17 @@
     pkgs.python311
     pkgs.uv
   ];
-
+  
   idx = {
-    extensions = [ "ms-python.python" ];
+    extensions = [ "ms-python.python"  "ms-python.debugpy"];
 
     workspace = {
       onCreate = {
         install = "uv venv .venv && source .venv/bin/activate && uv pip install -r requirements.txt gradio";
         default.openFiles = [ "README.md" "app.py" ];
+      };
+      onStart = {
+        activate = "source .venv/bin/activate";
       };
     };
 
